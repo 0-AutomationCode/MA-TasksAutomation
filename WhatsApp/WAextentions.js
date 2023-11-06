fb6d08ef647c9355db3ae01b07439d04bc6f6ee8
Mohammad Jouza <m.jozaa@yahoo.com>
2023-11-06 15:15:30 +0300

    whatsapp

---
+++ WhatsApp/WAextentions.js
@@ -0,0 +1,8 @@
+WhatsApp WEB
+https://web.whatsapp.com/
+
+Chrome Extension 1
+https://chrome.google.com/webstore/detail/wasender-free-plugin/heogilejknffekkbjdjmoamaehdblmnc
+
+Chrome Extension 2
+https://chrome.google.com/webstore/detail/wa-bulk-message-sender-wa/afgbckekjlfkfhklldgdndiagddhbohm
---
+++ WhatsApp/checkWALabel.js
@@ -0,0 +1,38 @@
+const mainDiv = $("#pane-side");
+
+const entireChat = mainDiv.children().children().children().children();
+
+let allNewMessagesNumbers = "";
+
+const allNewMessages = entireChat.map((i, oneChat) => {
+  // console.log(oneChat);
+  const [phoneNumber, time] = $(
+    $($(oneChat).children().children().children().children()[1]).children()[0]
+  ).children();
+  const phoneNumberArray = $(phoneNumber).text().split(" ");
+  timeText = $(time).text();
+
+  let phoneNumberText = "";
+
+  if (phoneNumberArray[0] === "+962") {
+    phoneNumberText =
+      phoneNumberArray[1][0] +
+      phoneNumberArray[phoneNumberArray.length - 2] +
+      phoneNumberArray[phoneNumberArray.length - 1];
+  } else {
+    // console.log(phoneNumberArray);
+    phoneNumberText = phoneNumberArray.join("").split("\n").join("").slice(1);
+  }
+  allNewMessagesNumbers = allNewMessagesNumbers + "\n" + phoneNumberText;
+
+  phoneNumberText = phoneNumberText;
+  const newMessage = {
+    phoneNumber: phoneNumberText,
+    time: timeText,
+  };
+  return newMessage;
+});
+
+console.log("allNewMessages: ", allNewMessages);
+
+console.log("allNewMessagesNumbers: ", allNewMessagesNumbers);
